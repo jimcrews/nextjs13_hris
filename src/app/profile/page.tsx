850c9908bed270883f0c35b7df76df7b567dd5b6
Jim Crews <crews.jim@gmail.com>
2023-09-24 19:55:18 +1000

    smoother signout

---
+++ src/app/profile/page.tsx
@@ -0,0 +1,38 @@
+'use client'
+
+import { useSession } from 'next-auth/react'
+import { signOut } from 'next-auth/react';
+import { redirect } from "next/navigation"
+
+import UserCard from "@/components/UserCard"
+import { Button } from "@/components/ui/button";
+
+
+export default function ProfilePage() {
+
+    const { data: session } = useSession({
+        required: true,
+        onUnauthenticated() {
+            redirect('/api/auth/signin?callbackUrl=/')
+        }
+    })
+
+    return (
+        <section className="flex flex-col gap-6">
+            
+        {!session?.user ? (
+            <>
+            <UserCard user={session?.user} pagetype={"Profile"} />
+
+            <Button onClick={() => signOut()}>Sign Out</Button>
+            </>
+        ): (
+            <p className="flex flex-col items-center mt-12">please wait..</p>
+        )}
+
+
+
+        </section>
+    )
+
+}
--- src/components/ProfileButton.tsx
+++ src/components/ProfileButton.tsx
@@ -1,39 +1,19 @@
 import Link from "next/link"
-import { signOut } from 'next-auth/react';
-import {
-    DropdownMenu,
-    DropdownMenuContent,
-    DropdownMenuItem,
-    DropdownMenuLabel,
-    DropdownMenuSeparator,
-    DropdownMenuTrigger,
-  } from "@/components/ui/dropdown-menu";
-  import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
-  
-  type Props = {
-    profileImageUrl: string,
-  }
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
-  const ProfileButton = ({ profileImageUrl }: Props) => {
-    return (
-      <DropdownMenu>
-        <DropdownMenuTrigger>
-          <Avatar>
-            {profileImageUrl ? <AvatarImage src={profileImageUrl} /> : <AvatarImage src="/assets/images/avatar_default.png" />}
-            <AvatarFallback>CN</AvatarFallback>
-          </Avatar>
-        </DropdownMenuTrigger>
-        <DropdownMenuContent>
-          <DropdownMenuLabel>My Account</DropdownMenuLabel>
-          <DropdownMenuSeparator />
-          <DropdownMenuItem className="cursor-pointer">Profile</DropdownMenuItem>
-          <DropdownMenuItem className="cursor-pointer">Billing</DropdownMenuItem>
-          <DropdownMenuItem className="cursor-pointer">Subscription</DropdownMenuItem>
-          <DropdownMenuSeparator />
-          <DropdownMenuItem className="cursor-pointer" onClick={() => signOut()}>Sign Out</DropdownMenuItem>
-        </DropdownMenuContent>
-      </DropdownMenu>
-    );
-  };
-  
-  export default ProfileButton;
+type Props = {
+  profileImageUrl: string,
+}
+
+const ProfileButton = ({ profileImageUrl }: Props) => {
+  return (
+    <Link href="/profile">
+      <Avatar>
+        {profileImageUrl ? <AvatarImage src={profileImageUrl} /> : <AvatarImage src="/assets/images/avatar_default.png" />}
+        <AvatarFallback>CN</AvatarFallback>
+      </Avatar>
+    </Link>
+  );
+};
+
+export default ProfileButton;
